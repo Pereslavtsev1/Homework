71121a12ff5367dd8b738e121b59c35f119e164c
Pereslavtsev Vladimir <vovapereslavtsev@gmail.com>
2025-04-21 16:09:07 +0300

    data types support added

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -1,6 +1,29 @@
 import { Check, Database, Globe, LinkIcon } from "lucide-react";
 import Button from "./Button";
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect, useCallback } from "react";
+
+const dataTypes = [
+  { value: "email", label: "Email" },
+  { value: "phone", label: "Phone" },
+  { value: "birthdate", label: "Birthdate" },
+  { value: "passport", label: "Passport (series and number)" },
+  { value: "zip", label: "Postal Code" },
+  { value: "country", label: "Country (Russia, USA, UK)" },
+  { value: "address", label: "Address" },
+  { value: "snils", label: "SNILS" },
+  { value: "inn", label: "INN" },
+  { value: "cardNumber", label: "Bank Card Number" },
+  { value: "iban", label: "IBAN" },
+  { value: "paymentAmount", label: "Payment Amount" },
+  { value: "vin", label: "VIN" },
+  { value: "orderDate", label: "Order Date" },
+  { value: "time", label: "Time" },
+  { value: "url", label: "URL" },
+  { value: "carNumber", label: "Car Number" },
+  { value: "password", label: "Password" },
+  { value: "macAddress", label: "MAC Address" },
+  { value: "coordinates", label: "Coordinates" },
+];
 
 const Form = () => {
   const [selectedItem, setSelectedItem] = useState("");
@@ -8,32 +31,10 @@ const Form = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const dropdownRef = useRef(null);
 
-  const items = [
-    { value: "email", label: "Email" },
-    { value: "phone", label: "Phone" },
-    { value: "birthdate", label: "Birthdate" },
-    { value: "passport", label: "Passport (series and number)" },
-    { value: "zip", label: "Postal Code" },
-    { value: "country", label: "Country (Russia, USA, UK)" },
-    { value: "address", label: "Address" },
-    { value: "snils", label: "SNILS" },
-    { value: "inn", label: "INN" },
-    { value: "cardNumber", label: "Bank Card Number" },
-    { value: "iban", label: "IBAN" },
-    { value: "paymentAmount", label: "Payment Amount" },
-    { value: "vin", label: "VIN" },
-    { value: "orderDate", label: "Order Date" },
-    { value: "time", label: "Time" },
-    { value: "url", label: "URL" },
-    { value: "carNumber", label: "Car Number" },
-    { value: "password", label: "Password" },
-    { value: "macAddress", label: "MAC Address" },
-    { value: "coordinates", label: "Coordinates" },
-  ];
-
-  const filteredItems = items.filter((item) =>
-    item.label.toLowerCase().includes(searchTerm.toLowerCase()),
-  );
+  const filteredItems = useCallback(() => {
+    const term = searchTerm.toLowerCase();
+    return dataTypes.filter((item) => item.label.toLowerCase().includes(term));
+  }, [searchTerm]);
 
   const handleItemClick = (item) => {
     setSelectedItem(item.label);
@@ -60,6 +61,7 @@ const Form = () => {
         <Globe className="text-foreground size-5" />
         <h1 className="text-lg font-bold">URL Data Validator</h1>
       </div>
+
       <div className="space-y-2">
         <label htmlFor="url" className="flex items-center gap-2 font-medium">
           <LinkIcon className="text-foreground size-4" />
@@ -77,6 +79,7 @@ const Form = () => {
           </div>
         </div>
       </div>
+
       <div className="space-y-2">
         <label
           htmlFor="data-type"
@@ -101,8 +104,8 @@ const Form = () => {
           />
           {isDropdownOpen && (
             <div className="bg-background border-foreground/30 absolute left-0 z-10 mt-1 flex max-h-40 w-full flex-col gap-0.5 overflow-y-auto rounded-md border p-1 shadow-lg">
-              {filteredItems.length > 0 ? (
-                filteredItems.map((item) => (
+              {filteredItems().length > 0 ? (
+                filteredItems().map((item) => (
                   <button
                     key={item.label}
                     className="rounded px-4 py-1 text-start text-xs hover:bg-[#27272a]"
@@ -120,6 +123,7 @@ const Form = () => {
           )}
         </div>
       </div>
+
       <div>
         <Button className="w-full">
           <Check />
