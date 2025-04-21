dc84088b6a3d74ed201d0d46a576fce7af0e5202
Pereslavtsev Vladimir <vovapereslavtsev@gmail.com>
2025-04-21 16:11:28 +0300

    cors accept all config added

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.endpoints.generate_data import generate_data_router
 
 version = "v1"
 app = FastAPI(version=version)
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(generate_data_router, prefix=f"/api/{version}/generate-data")
